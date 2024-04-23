9744af5821105f5804b962d31c12ae10637b37f2
RohunRanjith <rohun.ranjith@gmail.com>
2024-04-23 10:48:05 +0200

    Passives start

---
+++ Assets/Scripts/Passive.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passive : ScriptableObject
+{
+    public string passiveName;
+    public string passiveDescription;
+    public int extraBurrowDamage;
+    public int extraCollectionRange;
+}
--- Assets/Scripts/RulesUI.cs
+++ Assets/Scripts/RulesUI.cs
@@ -9,10 +9,14 @@ using UnityEngine.EventSystems;
 public class RulesUI : SingletonBehaviour<RulesUI>
 {
     [SerializeField] CanvasGroup _rulesUICanvasGroup;
-
     [SerializeField] RuleOptionUI _ruleOptionPrefab;
     [SerializeField] Transform _ruleOptionsParent;
     List<RuleOptionUI> _currentRuleOptions = new List<RuleOptionUI>();
+
+    [Header("Passives")]
+    [SerializeField] CanvasGroup _passivesUICanvasGroup;
+    [SerializeField] Transform _passiveOptionsParent;
+    List<Passive> _availablePassives = new List<Passive>();
     void Start()
     {
         
@@ -60,7 +64,27 @@ public class RulesUI : SingletonBehaviour<RulesUI>
         _currentRuleOptions.Clear();
         foreach (RuleOptionUI option in currentOptions)
             Destroy(option.gameObject);
+    }
+
+    IEnumerator ActivatePassiveUIRoutine()
+    {
+        Time.timeScale = 0f;
+        _rulesUICanvasGroup.alpha = 1f;
+        _rulesUICanvasGroup.interactable = true;
+
+        yield return new WaitForSecondsRealtime(0.25f);
+
+        for (int i = 0; i < 3; i++)
+        {
+            RuleOptionUI option = Instantiate(_ruleOptionPrefab, _ruleOptionsParent);
+            _currentRuleOptions.Add(option);
+            option.SetUpRuleOption();
 
 
+            yield return new WaitForSecondsRealtime(0.15f);
+        }
+
+        if (_currentRuleOptions.Count > 0)
+            EventSystem.current.SetSelectedGameObject(_currentRuleOptions[0].OptionButton.gameObject);
     }
 }
